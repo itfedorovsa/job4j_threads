81f9e206baa17ee70751a26392dd095b277a52c5
FedorovSA <itfedorovsa@gmail.com>
2022-08-17 15:34:00 +0300

    Thread without shared resources (update)

--- src/main/java/ru/job4j/ref/ShareNotSafe.java
+++ src/main/java/ru/job4j/ref/ShareNotSafe.java
@@ -5,7 +5,7 @@ public class ShareNotSafe {
         UserCache cache = new UserCache();
         User user = User.of("name");
         cache.add(user);
-        Thread first = new Thread( () -> user.setName("rename"));
+        Thread first = new Thread(() -> user.setName("rename"));
         first.start();
         first.join();
         System.out.println(cache.findById(1).getName());
