1921efd6c7e882427ff2c7df5434cbb403c57655
FedorovSA <itfedorovsa@gmail.com>
2022-08-17 14:31:34 +0300

    Immutable objects

---
+++ src/main/java/ru/job4j/linked/Node.java
@@ -0,0 +1,22 @@
+package ru.job4j.linked;
+
+public final class Node<T> {
+    private final Node<T> next;
+    private final T value;
+
+    public Node(T value, Node<T> next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public Node<T> getNext() {
+        Node<T> copy = next;
+        return copy;
+    }
+
+    public T getValue() {
+        T copy = value;
+        return copy;
+    }
+
+}
