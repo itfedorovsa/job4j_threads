f346ccd0476b61f2cfc2a93fd7b2a0a8cf776ff3
FedorovSA <itfedorovsa@gmail.com>
2022-08-17 20:21:09 +0300

    Visibility. Shared resource outside the critical section

---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,27 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public final class ParseFile {
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    public String content(Predicate<Character> filter) {
+        StringBuilder output = new StringBuilder();
+        int data;
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            while ((data = in.read()) > 0) {
+                if (filter.test((char) data)) {
+                    output.append((char) data);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+}
