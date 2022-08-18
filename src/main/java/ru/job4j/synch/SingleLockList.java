7213f78ea32e233462a0301d3c8f655ec2718d8f
FedorovSA <itfedorovsa@gmail.com>
2022-08-18 23:01:42 +0300

    Thread Safe Dynamic List

---
+++ src/main/java/ru/job4j/synch/SingleLockList.java
@@ -0,0 +1,36 @@
+package ru.job4j.synch;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+@ThreadSafe
+public class SingleLockList<T> implements Iterable<T> {
+    @GuardedBy("this")
+    private final List<T> list;
+
+    public SingleLockList(List<T> list) {
+        this.list = Collections.synchronizedList(list);
+    }
+
+    public synchronized void add(T value) {
+        list.add(value);
+    }
+
+    public synchronized T get(int index) {
+        return copy(list).get(index);
+    }
+
+    private synchronized List<T> copy(List<T> list) {
+        return new ArrayList<>(list);
+    }
+
+    @Override
+    public synchronized Iterator<T> iterator() {
+        return copy(list).iterator();
+    }
+}
